ee6da81d6e3f967426912b4bc87032148dcf14d9
Admin <Admin@DELL>
2022-09-01 23:07:57 +0530

    first commit

---
+++ SpringWebMVCPostman/src/main/java/com/example/model/Response.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+public class Response 
+{
+	private String data;
+	private int responseCode;
+	private String status;
+	
+	//getters and setters methods
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public int getResponseCode() {
+		return responseCode;
+	}
+	public void setResponseCode(int responseCode) {
+		this.responseCode = responseCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+
+}
